6b67e51ee7dab45a611b1afb49a559191697d9cc
Your Name <you@example.com>
2024-02-09 14:41:00 -0500

    first commit

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <>
-      <div className="items-center justify-center">FOOD APP DELIVERY</div>
+      <div className="items-center justify-center bg-gray-200">
+        FOOD APP DELIVERY
+      </div>
     </>
   );
 }
