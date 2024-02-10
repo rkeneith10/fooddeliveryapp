d1510848fc540909326f891aab8b27f179d26b75
Your Name <you@example.com>
2024-02-09 20:41:25 -0500

    frontend working

--- components/footer.js
+++ components/footer.js
@@ -2,10 +2,10 @@
 
 const Footer = () => {
   return (
-    <footer className="bg-[#1D4851] text-white py-4 fixed mt-auto  ">
+    <footer className="bg-[#1D4851] text-white py-4    ">
       <div className="container mx-auto text-center">
         <p>&copy; 2024 Food App | All rights reserved.</p>
-        <p>Created by Keneith Salnave Romain.</p>
+        <p>Created by Keneith Salnave.</p>
       </div>
     </footer>
   );
