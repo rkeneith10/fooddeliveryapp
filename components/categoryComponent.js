6857dc66df077424153dd238601141ba532ac17e
Your Name <you@example.com>
2024-02-10 14:52:06 -0500

    fetch categories from database

--- components/categoryComponent.js
+++ components/categoryComponent.js
@@ -1,6 +1,6 @@
 import { CldImage } from "next-cloudinary";
 
-export default function categoryComponent(cloudinary_id, category) {
+export default function CategoryComponent(cloudinary_id, category) {
   return (
     <div className="bg-white rounded shadow-md justify-center items-center h-[100px] w-[100px] p-5 flex flex-col">
       <CldImage src={cloudinary_id} className="w-full h-[70px] rounded-full " />
