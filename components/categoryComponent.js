f14cbbe60412564cf8e11132adff0880bdbc49c6
Your Name <you@example.com>
2024-02-10 15:31:52 -0500

    fetch categories from database

--- components/categoryComponent.js
+++ components/categoryComponent.js
@@ -2,12 +2,12 @@ import { CldImage } from "next-cloudinary";
 
 export default function CategoryComponent({ imageUrl, category }) {
   return (
-    <div className="bg-white rounded-lg overflow-hidden shadow-lg mx-2 my-4 h-[250px]">
-      <div className="px-6 py-2">
+    <div className="bg-white rounded-lg overflow-hidden shadow-lg mx-2 my-4 h-[240px]">
+      <div className="px-6 py-2 mt-4">
         <CldImage src={imageUrl} className="w-full h-[160px] rounded-full" />
       </div>
       <div className="px-6 py-4">
-        <div className="font-normal text-xl mb-1 text-center">{category}</div>
+        <div className="font-medium text-xl mb-1 text-center">{category}</div>
       </div>
     </div>
   );
