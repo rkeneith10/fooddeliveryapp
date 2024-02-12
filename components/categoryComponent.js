ff58192122c6062f37c8d3611627fd9d5a4734c2
Your Name <you@example.com>
2024-02-12 12:56:52 -0500

    fixed image categories

--- components/categoryComponent.js
+++ components/categoryComponent.js
@@ -4,9 +4,9 @@ export default function CategoryComponent({ imageUrl, category }) {
   return (
     <div className="bg-white rounded-lg overflow-hidden shadow-lg mx-2 my-2 h-[250px] md:h-[240px]">
       <div className="px-6 py-2 mt-4">
-        <CldImage src={imageUrl} className="w-[150px] h-[150px] rounded-lg" />
+        <CldImage src={imageUrl} className="w-[150px] h-[150px] rounded-full" />
       </div>
-      <div className="px-6 py-4">
+      <div className="px-6 py-2">
         <div className="font-medium text-xl mb-1 text-center">{category}</div>
       </div>
     </div>
