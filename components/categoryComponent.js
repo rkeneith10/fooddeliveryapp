acaea08ef52e9422908d6f9791cba21112d5f387
Your Name <you@example.com>
2024-02-12 12:52:13 -0500

    back using useEfect

--- components/categoryComponent.js
+++ components/categoryComponent.js
@@ -4,10 +4,10 @@ export default function CategoryComponent({ imageUrl, category }) {
   return (
     <div className="bg-white rounded-lg overflow-hidden shadow-lg mx-2 my-2 h-[250px] md:h-[240px]">
       <div className="px-6 py-2 mt-4">
-        <CldImage src={imageUrl} className="w-full h-[160px] rounded-lg" />
+        <CldImage src={imageUrl} className="w-[150px] h-[150px] rounded-lg" />
       </div>
       <div className="px-6 py-4">
-        <div className="font-medium text-xl mb-1 text-center">{category}es</div>
+        <div className="font-medium text-xl mb-1 text-center">{category}</div>
       </div>
     </div>
   );
