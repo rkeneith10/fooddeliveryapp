f9db7ee9e95ff46ef51fd4228419168492860de1
Your Name <you@example.com>
2024-02-12 20:15:03 -0500

    dropdown menu

--- components/categoryComponent.js
+++ components/categoryComponent.js
@@ -3,11 +3,13 @@ import { CldImage } from "next-cloudinary";
 export default function CategoryComponent({ imageUrl, category }) {
   return (
     <div className="bg-white rounded-lg overflow-hidden shadow-lg mx-2 my-2 h-[250px] md:h-[240px]">
-      <div className="px-6 py-2 mt-4">
-        <CldImage src={imageUrl} className="w-[150px] h-[150px] rounded-full" />
+      <div className="px-6 py-2 mt-2">
+        <CldImage src={imageUrl} className="w-[150px] h-[150px] rounded-lg" />
       </div>
       <div className="px-6 py-2">
-        <div className="font-medium text-xl mb-1 text-center">{category}</div>
+        <div className="font-medium text-medium mb-1 text-center">
+          {category}
+        </div>
       </div>
     </div>
   );
