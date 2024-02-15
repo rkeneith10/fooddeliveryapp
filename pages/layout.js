e76a93bc1b3ba4100dcb6a02b78ea98d2a2837e8
Your Name <you@example.com>
2024-02-15 15:35:39 -0500

    adding layout

---
+++ pages/layout.js
@@ -0,0 +1,20 @@
+import { Montserrat } from "next/font/google";
+import "../globals.css";
+
+const montserrat = Montserrat({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Food App Delivery",
+  description: "Food Restaurant application",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={montserrat.className}>
+        <p>LOl</p>
+        {children}
+      </body>
+    </html>
+  );
+}
