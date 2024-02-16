c98a3bca93e56e0eff8c6e983b4be1f82dc7bf00
Your Name <you@example.com>
2024-02-16 13:19:01 -0500

    layout

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import Head from "next/head";
 import Link from "next/link";
 import "tailwindcss/tailwind.css";
 import BackImage1 from "../public/images/foodapp.PNG";
+import Layout from "./layout";
 export async function getStaticProps() {
   const response = await fetch(
     "https://fooddelivery-kappa.vercel.app/api/categories"
@@ -39,72 +40,74 @@ export async function getStaticProps() {
 export default function index({ categories, restaurants }) {
   return (
     <>
-      <div>
-        <Head>
-          <title>Food App Delivery</title>
-          <meta
-            name="description"
-            content="The App that will change your life"
-          />
-          {/* Add other meta tags here */}
-        </Head>
-        {/* <Navbar /> */}
-        <div
-          id="top"
-          className="h-[400px]  max-w-screen-2xl mx-auto flex flex-col justify-center p-7 lg:p-40  bg-center bg-cover bg-no-repeat "
-          style={{ backgroundImage: `url(${BackImage1.src})` }}
-        >
-          <div className="max-w-sceen">
-            <h1 className="text-3xl  text-white font-bold ">
-              FOOD APP DELIVERY
-            </h1>
-            <br />
-            <h1 className="text-xl   text-white font-normal ">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-              cursus enim dapibus enim faucibus laoreet vel at sapien.
-              Pellentesque hendrerit, ligula eget dignissim sagittis, nisi ipsum
-              facilisis dolor, quis ultrices orci leo dignissim ligula.
-            </h1>
-            <br />
+      <Layout>
+        <div>
+          <Head>
+            <title>Food App Delivery</title>
+            <meta
+              name="description"
+              content="The App that will change your life"
+            />
+            {/* Add other meta tags here */}
+          </Head>
+          {/* <Navbar /> */}
+          <div
+            id="top"
+            className="h-[400px]  max-w-screen-2xl mx-auto flex flex-col justify-center p-7 lg:p-40  bg-center bg-cover bg-no-repeat "
+            style={{ backgroundImage: `url(${BackImage1.src})` }}
+          >
+            <div className="max-w-sceen">
+              <h1 className="text-3xl  text-white font-bold ">
+                FOOD APP DELIVERY
+              </h1>
+              <br />
+              <h1 className="text-xl   text-white font-normal ">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+                cursus enim dapibus enim faucibus laoreet vel at sapien.
+                Pellentesque hendrerit, ligula eget dignissim sagittis, nisi
+                ipsum facilisis dolor, quis ultrices orci leo dignissim ligula.
+              </h1>
+              <br />
+            </div>
           </div>
-        </div>
-        <div className="p-10 bg-gray-100">
-          <div className="text-xl font-semibold text-center mb-3">
-            <span>Popular Categories</span>
-          </div>
-          <div className="grid grid-cols-2 lg:grid-cols-6 gap-1">
-            {categories.map((cat) => (
-              <div key={cat._id}>
-                <CategoryComponent
-                  category={cat.category}
-                  imageUrl={cat.imageUrl}
-                />
-              </div>
-            ))}
-          </div>
-        </div>
-
-        <div className="p-10 bg-gray-50">
-          <div className="text-xl font-semibold text-center mb-3">
-            <span>Nearby Restaurants</span>
-          </div>
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
-            {restaurants.map((resto) => (
-              <div key={resto._id}>
-                <Link href={`/restaurants/${resto._id}`}>
-                  <RestaurantComponent
-                    restaurant_name={resto.restaurant_name}
-                    telephone={resto.telephone}
-                    imageUrl={resto.imageUrl}
-                    adress={resto.adress}
+          <div className="p-10 bg-gray-100">
+            <div className="text-xl font-semibold text-center mb-3">
+              <span>Popular Categories</span>
+            </div>
+            <div className="grid grid-cols-2 lg:grid-cols-6 gap-1">
+              {categories.map((cat) => (
+                <div key={cat._id}>
+                  <CategoryComponent
+                    category={cat.category}
+                    imageUrl={cat.imageUrl}
                   />
-                </Link>
-              </div>
-            ))}
+                </div>
+              ))}
+            </div>
           </div>
+
+          <div className="p-10 bg-gray-50">
+            <div className="text-xl font-semibold text-center mb-3">
+              <span>Nearby Restaurants</span>
+            </div>
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+              {restaurants.map((resto) => (
+                <div key={resto._id}>
+                  <Link href={`/restaurants/${resto._id}`}>
+                    <RestaurantComponent
+                      restaurant_name={resto.restaurant_name}
+                      telephone={resto.telephone}
+                      imageUrl={resto.imageUrl}
+                      adress={resto.adress}
+                    />
+                  </Link>
+                </div>
+              ))}
+            </div>
+          </div>
+          {/* <Footer /> */}
         </div>
-        {/* <Footer /> */}
-      </div>
+      </Layout>
     </>
   );
 }
--- pages/restaurants/[restaurantId].js
+++ pages/restaurants/[restaurantId].js
@@ -1,12 +1,13 @@
 "use client";
-import Footer from "@/components/footer";
+// import Footer from "@/components/footer";
 import MenuItemComponent from "@/components/menuItemComponent";
-import Navbar from "@/components/navbar";
+// import Navbar from "@/components/navbar";
 import axios from "axios";
 import Head from "next/head";
 import { useEffect, useState } from "react";
 import "tailwindcss/tailwind.css";
 import BackgoundImg from "../../public/images/resto.JPG";
+import Layout from "../layout";
 
 function RestaurantDetail({ data, error }) {
   const [activeCategory, setActiveCategory] = useState(null);
@@ -33,77 +34,81 @@ function RestaurantDetail({ data, error }) {
 
   return (
     <div>
-      <Head>
-        <title>{data.restaurant_name}</title>
-        <meta name="description" content={data.restaurant_name} />
-        {/* Add other meta tags here */}
-      </Head>
-      <Navbar />
-      <div
-        id="top"
-        className="h-[220px] max-w-screen-2xl mx-auto flex flex-col justify-center p-7 lg:p-40 bg-center bg-cover bg-no-repeat relative"
-        style={{
-          backgroundImage: `url(${BackgoundImg.src})`,
-        }}
-      >
-        {/* Overlay to improve text readability */}
+      <Layout>
+        <Head>
+          <title>{data.restaurant_name}</title>
+          <meta name="description" content={data.restaurant_name} />
+          {/* Add other meta tags here */}
+        </Head>
+        {/* <Navbar /> */}
         <div
-          className="absolute inset-0 bg-black opacity-50"
-          aria-hidden="true"
-        />
+          id="top"
+          className="h-[220px] max-w-screen-2xl mx-auto flex flex-col justify-center p-7 lg:p-40 bg-center bg-cover bg-no-repeat relative"
+          style={{
+            backgroundImage: `url(${BackgoundImg.src})`,
+          }}
+        >
+          {/* Overlay to improve text readability */}
+          <div
+            className="absolute inset-0 bg-black opacity-50"
+            aria-hidden="true"
+          />
 
-        <div className="max-w-sceen relative z-10">
-          <h1 className="text-3xl text-white font-bold ">
-            {data.restaurant_name}
-          </h1>
-          <br />
-          <h4 className="text-xl text-white font-normal ">{data.adress}</h4>
-          <h4 className="text-xl text-white font-normal ">{data.telephone}</h4>
-          <br />
-        </div>
-      </div>
-
-      {categoriesWithMenus.length === 0 ? (
-        <div className="bg-gray-100 p-4 text-center">
-          This restaurant currently has no menu available.
-        </div>
-      ) : (
-        <div className="flex overflow-x-auto space-x-4 p-4 ">
-          {categoriesWithMenus.map((category, index) => (
-            <button
-              key={index}
-              onClick={() => setActiveCategory(category)}
-              className={`p-2 rounded ${
-                category === activeCategory
-                  ? "bg-[#4CAF50] text-white"
-                  : "bg-gray-300 text-[#4CAF50]"
-              }`}
-            >
-              {category}
-            </button>
-          ))}
-        </div>
-      )}
-
-      {activeCategory && (
-        <div className="bg-gray-200">
-          <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-2">
-            {data.menus
-              .filter((menu) => menu.category === activeCategory)
-              .map((menu, menuIndex) => (
-                <div key={menuIndex} className="p-4 ">
-                  <MenuItemComponent
-                    item_name={menu.item_name}
-                    imageUrl={menu.imageUrl}
-                    description={menu.description}
-                    price={menu.price}
-                  />
-                </div>
-              ))}
+          <div className="max-w-sceen relative z-10">
+            <h1 className="text-3xl text-white font-bold ">
+              {data.restaurant_name}
+            </h1>
+            <br />
+            <h4 className="text-xl text-white font-normal ">{data.adress}</h4>
+            <h4 className="text-xl text-white font-normal ">
+              {data.telephone}
+            </h4>
+            <br />
           </div>
         </div>
-      )}
-      <Footer />
+
+        {categoriesWithMenus.length === 0 ? (
+          <div className="bg-gray-100 p-4 text-center">
+            This restaurant currently has no menu available.
+          </div>
+        ) : (
+          <div className="flex overflow-x-auto space-x-4 p-4 ">
+            {categoriesWithMenus.map((category, index) => (
+              <button
+                key={index}
+                onClick={() => setActiveCategory(category)}
+                className={`p-2 rounded ${
+                  category === activeCategory
+                    ? "bg-[#4CAF50] text-white"
+                    : "bg-gray-300 text-[#4CAF50]"
+                }`}
+              >
+                {category}
+              </button>
+            ))}
+          </div>
+        )}
+
+        {activeCategory && (
+          <div className="bg-gray-200">
+            <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-2">
+              {data.menus
+                .filter((menu) => menu.category === activeCategory)
+                .map((menu, menuIndex) => (
+                  <div key={menuIndex} className="p-4 ">
+                    <MenuItemComponent
+                      item_name={menu.item_name}
+                      imageUrl={menu.imageUrl}
+                      description={menu.description}
+                      price={menu.price}
+                    />
+                  </div>
+                ))}
+            </div>
+          </div>
+        )}
+        {/* <Footer /> */}
+      </Layout>
     </div>
   );
 }
