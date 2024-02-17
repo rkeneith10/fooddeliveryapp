7497adda1fe2a6e2452cdf71575667bc32fdfc81
Your Name <you@example.com>
2024-02-17 15:55:19 -0500

    add to cart

--- components/navbar.js
+++ components/navbar.js
@@ -93,7 +93,7 @@ export default function Navbar() {
         <div className="pl-[80px]   md:pr-10">
           <ShoppingCartIcon className="text-[#4CAF50] h-9 w-9" />
           {cartItemCount > 0 && (
-            <span className="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white transform translate-x-1/2 -translate-y-1/2 bg-[#4CAF50] rounded-full">
+            <span className="absolute top-20 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white transform translate-x-1/2 -translate-y-1/2 bg-[#4CAF50] rounded-full">
               {cartItemCount}
             </span>
           )}
