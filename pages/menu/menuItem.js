bb5a521f6a0e6ca5dbe050ee81c1e27bfc985db7
Your Name <you@example.com>
2024-02-17 15:27:29 -0500

    add toaster

--- pages/menu/menuItem.js
+++ pages/menu/menuItem.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { CldImage } from "next-cloudinary";
 import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Layout from "../layout";
 
 function MenuItem({ data, error }) {
@@ -19,29 +21,26 @@ function MenuItem({ data, error }) {
 
   const addToCart = () => {
     const cartItem = {
-      id: data._id, // Ajoutez ici l'identifiant unique de l'élément
+      id: data._id,
       name: data.item_name,
       price: data.price,
       quantity: count,
       specialRequest: specialRequest,
     };
 
-    // Récupérez le panier existant depuis le stockage local
     const existingCart = JSON.parse(localStorage.getItem("cart")) || [];
 
-    // Ajoutez le nouvel élément au panier
     const updatedCart = [...existingCart, cartItem];
 
-    // Mettez à jour le panier dans le stockage local
     localStorage.setItem("cart", JSON.stringify(updatedCart));
 
-    // Réinitialisez les champs
     setCount(1);
     setSpecialRequest("");
 
-    alert("Item added to cart!");
+    // alert("Item added to cart!");
+    toast("Item added to cart!");
   };
-
+  const notify = () => toast("Wow so easy!");
   if (error) {
     return <p>Error loading post: {error}</p>;
   }
@@ -53,16 +52,20 @@ function MenuItem({ data, error }) {
   return (
     <div>
       <Layout>
+        <div>
+          {" "}
+          <ToastContainer />
+        </div>
         <div className=" h-auto  max-w-screen-2xl mx-auto flex flex-col justify-center items-center bg-gray-200  ">
           <div className="pt-5 pl-10 pr-5 bg-white rounded-md shadow-sm sm:w-full lg:w-[440px]  h-auto flex flex-col">
             <div>
-              <CldImage src={data.imageUrl} className="w-full h-[200px] mb-4" />
+              <CldImage src={data.imageUrl} className="w-full h-[200px] mb-2" />
 
               <div className="p-4">
                 <h3 className="font-bold text-xl text-gray-900 mb-2">
                   {data.item_name}
                 </h3>
-                <div className="flex flex-row justify-between mb-4">
+                <div className="flex flex-row justify-between mb-2">
                   <h4 className="font-bold ">Price</h4>
                   <p className="font-bold">
                     <span className="text-[#4CAF50]">HTG </span>
@@ -70,11 +73,11 @@ function MenuItem({ data, error }) {
                   </p>
                 </div>
 
-                <div class="border-t border-gray-300 mb-4"></div>
+                <div class="border-t border-gray-300 mb-2"></div>
 
                 <div className="text-md font-semibold">Special request</div>
 
-                <div className="mb-3">
+                <div className="mb-2">
                   <textarea
                     value={specialRequest}
                     onChange={(e) => setSpecialRequest(e.target.value)}
@@ -83,7 +86,7 @@ function MenuItem({ data, error }) {
                   ></textarea>
                 </div>
 
-                <div className="flex flex-row justify-between mb-4">
+                <div className="flex flex-row justify-between mb-2">
                   <button
                     onClick={decrement}
                     className="bg-gray-300 px-4 py-2 text-white text-xl  rounded-sm"
