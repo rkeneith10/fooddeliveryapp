7b262e89191b289e3e9e48fee7f86f859e6da260
Your Name <you@example.com>
2024-02-17 16:41:01 -0500

    add to cart

--- components/categoryComponent.js
+++ components/categoryComponent.js
@@ -3,7 +3,7 @@ import { CldImage } from "next-cloudinary";
 export default function CategoryComponent({ imageUrl, category }) {
   return (
     <div className="bg-white rounded-lg overflow-hidden shadow-lg mx-2 my-2 h-[250px] md:h-[240px]">
-      <div className="h-100 w-100 border border-red-200 relative overflow-hidden  mb-1">
+      <div className="h-[150px] w-[150px] border border-red-200 relative overflow-hidden  mb-1">
         <CldImage
           src={imageUrl}
           className="h-full w-full object-cover object-center"
