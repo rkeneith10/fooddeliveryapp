520d81d139d37fb61a06ae97dde283175694fc12
Your Name <you@example.com>
2024-02-18 09:06:16 -0500

    icons change color

--- components/navbar.js
+++ components/navbar.js
@@ -113,7 +113,7 @@ export default function Navbar() {
           )}
 
           {showCartPopUp && (
-            <div className="absolute top-15 right-28 mt-2 py-2 w-50 bg-white border rounded-lg shadow-xl z-10">
+            <div className="absolute top-11 right-20 mt-2 py-2 w-50 bg-white border rounded-lg shadow-xl z-10">
               <ul>
                 {getCartItems().map((item, index) => (
                   <li key={index} className="px-4 py-2 border-b">
