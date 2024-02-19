1550a371b4619cad49777722a7a54839e873bb3a
Your Name <you@example.com>
2024-02-19 12:04:39 -0500

    tableau hover

--- pages/orders/index.js
+++ pages/orders/index.js
@@ -32,9 +32,9 @@ export default function Orders() {
         </div>
 
         <div className="bg-gray-100">
-          <div className="pt-10 pl-15 pr-15">
+          <div className="pt-10 pl-20 pr-20">
             <div className="relative overflow-x-auto shadow-md sm:rounded-lg ">
-              <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+              <table className="w-full text-sm text-center rtl:text-right text-gray-500 dark:text-gray-400">
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                   <tr>
                     <th scope="col" className="px-6 py-3">
