fea5ca919df5c99d629c6891d663d189673016fe
Your Name <you@example.com>
2024-02-19 19:10:34 -0500

    condition to display info about the cart

--- pages/orders/index.js
+++ pages/orders/index.js
@@ -164,7 +164,7 @@ export default function Orders() {
                 </div>
                 <div className="flex justify-end mb-5 items-center">
                   <h2 className="mr-4">Payment method:</h2>
-                  <div classNAme="flex ml-auto">
+                  <div className="flex">
                     <input
                       type="radio"
                       id="pay"
