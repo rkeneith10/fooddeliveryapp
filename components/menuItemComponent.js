6d3bfd70a36812a31d8e5d42573600be3da4cc41
Your Name <you@example.com>
2024-02-21 19:17:02 -0500

    change mneu component

--- components/menuItemComponent.js
+++ components/menuItemComponent.js
@@ -8,14 +8,14 @@ export default function MenuItemComponent({
   price,
   randomRating,
 }) {
-  const renderStarRating = (rating) => {
+  const renderStarRating = () => {
     const stars = [];
     for (let i = 0; i < 5; i++) {
       stars.push(
         <FaStar
           key={i}
           className={`w-3 h-3 ${
-            i <= rating - 1 ? "text-yellow-500" : "text-gray-300"
+            i < 3 ? "text-yellow-500" : "text-gray-300"
           } inline-block`}
         />
       );
@@ -33,12 +33,10 @@ export default function MenuItemComponent({
       <div className="flex flex-col items-start">
         <h5 className="text-md font-semibold mb-1">{item_name}</h5>
         <div className="flex justify-between">
-          <p className="text-gray-800 font-semibold">
+          <p className="text-gray-800 font-semibold text-sm">
             <span className="text-[#4CAF50]">HTG </span> {price}
           </p>
-          <div className="flex items-center">
-            {renderStarRating(randomRating)}
-          </div>
+          <div className="flex items-center">{renderStarRating()}</div>
         </div>
       </div>
     </div>
