6b17cbd7bea0cb5d6f3ee9b2a2414ca6faf5b962
Your Name <you@example.com>
2024-02-21 19:34:33 -0500

    div horizontal

--- pages/index.js
+++ pages/index.js
@@ -74,9 +74,9 @@ export default function index({ categories, restaurants }) {
             <div className="text-xl font-semibold text-center mb-3">
               <span>Popular Categories</span>
             </div>
-            <div className="flex overflow-x-auto">
+            <div className="grid grid-cols-2 lg:grid-cols-6 gap-3">
               {categories.map((cat) => (
-                <div key={cat._id} className="flex-shrink-0 mr-3">
+                <div key={cat._id}>
                   <CategoryComponent
                     category={cat.category}
                     imageUrl={cat.imageUrl}
