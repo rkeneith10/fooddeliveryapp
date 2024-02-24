1e1182488caea2380398daf469d8d42e0484fb9e
Your Name <you@example.com>
2024-02-24 15:46:58 -0500

    function order

--- pages/api/orders/index.js
+++ pages/api/orders/index.js
@@ -4,12 +4,12 @@ import users from "../models/users";
 import connectDB from "../utils/database";
 
 const handler = async (req, res) => {
-  connectDB();
+  await connectDB();
 
   if (req.method === "POST") {
     const { restaurant_name, price, menu_item_name, quantite } = req.body;
 
-    const token = await req.headers["x-access-token"];
+    const token = req.cookies.token;
     const decoded = jwt.verify(token, process.env.SECRET_KEY);
     const user = await users
       .findOne({
@@ -23,7 +23,7 @@ const handler = async (req, res) => {
       menu_item_name: menu_item_name,
       quantite: quantite,
       delivery_adress: user.adress,
-      price: price * quantite,
+      price: price,
     });
     await newOrder.save();
 
