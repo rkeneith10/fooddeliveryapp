45c0378acf94ab8da11f2dfbd7c93b7d6a09949e
Your Name <you@example.com>
2024-02-24 15:11:02 -0500

    logout function done

--- components/navbar.js
+++ components/navbar.js
@@ -19,6 +19,7 @@ export default function Navbar() {
   };
   const logout = async () => {
     localStorage.removeItem("isLogin");
+    localStorage.removeItem("userinfo");
     setIsLogin(false);
   };
 
