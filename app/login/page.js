7861827d3dc517c088f03402d9867a01f87b7fee
Your Name <you@example.com>
2024-03-13 09:28:57 -0400

    test

--- app/login/page.js
+++ app/login/page.js
@@ -127,7 +127,7 @@ export default function Login() {
                 <svg
                   aria-hidden="true"
                   role="status"
-                  class="inline mr-2 w-4 h-4 text-gray-200 animate-spin dark:text-gray-600"
+                  class="inline mr-2 w-4 h-4 text-white animate-spin "
                   viewBox="0 0 100 101"
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
